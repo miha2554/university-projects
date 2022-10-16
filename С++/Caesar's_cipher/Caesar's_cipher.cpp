b4e6cfb163f54ddffe8299f0a3dfa16ff09472e4
des papillons dans le ventre <75795963+miha2554@users.noreply.github.com>
2022-10-16 23:21:53 +0400

    Add files via upload

---
+++ С++/Caesar's_cipher/Caesar's_cipher.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <locale>
+#include <windows.h>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    setlocale(LC_ALL, "Russian");
+    int step;
+    char inptext[255];
+    cout << "Input text - ";
+    cin.getline(inptext, 255);
+    cout << "Input shag - ";
+    cin >> step;
+    for (int i = 0; i < strlen(inptext); i++)
+    {
+        if (inptext[i] == 'я') {
+            inptext[i] = 'а';
+        }
+        if (inptext[i] >= 'а' && inptext[i] < 'я') {
+            inptext[i] += step;
+        }
+    }
+    cout << "Output -  " << inptext << endl;
+
+}
